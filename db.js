37a5b56118e17085616458f1c50efca9aadf8cea
Mirco <mirco.zeiss@gmail.com>
2013-05-01 22:38:31 +0200

    check for local or production env

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 
 var nano = require('nano');
 // not included in repo - use your own CouchDB instance
-var config = require('./config.prod.js');
+var config;
+if (process.env.NODE_ENV === 'production') {
+  config.user = process.env.user;
+  config.pw = process.env.pw;
+} else {
+  config = require('./config.prod.js');
+}
 
 var db = nano('https://' + config.user + ':' + config.pw + '@mirco.cloudant.com/blog-persona-example/');
 
