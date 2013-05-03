2cbc017a49ef92467783814c3ddabffde6846deb
Mirco <mirco.zeiss@gmail.com>
2013-05-03 19:48:11 +0200

    dry

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,11 @@
 
 var db = require('../db.js');
 
+/**
+ * GET /profile
+ *
+ * Identify user via session and get information from db.
+ */
 exports.get = function(req, res) {
   var email = req.session.email;
 
@@ -14,20 +19,26 @@ exports.get = function(req, res) {
 
 };
 
+/**
+ * POST /profile
+ *
+ * Handle post request and save username to db.
+ */
 exports.post = function(req, res) {
   var email = req.session.email;
+  var username = req.body.username;
 
   // get document from db
   db.get(email, function(err, doc) {
     if (err) console.log(err);
     // set username property
-    doc.username= req.body.username;
+    doc.username= username;
     // save document to db
     db.insert(doc, email, function(err, body) {
       res.render('profile', {
         title: 'Your profile',
         email: email,
-        username: req.body.username
+        username: username
       })
     })
   });
