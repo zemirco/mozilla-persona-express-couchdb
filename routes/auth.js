0ffd4574de37a41d176bcf5d194aef18dba65de3
Mirco <mirco.zeiss@gmail.com>
2013-05-03 19:44:13 +0200

    rm email route

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,29 @@
 var request = require('request');
 var db = require('../db.js');
 
+/**
+ * POST /auth/login
+ *
+ * Route is part of Persona integration. It is called by navigator.id.watch -> onlogin.
+ * Take the assertion sent from the client and verify against Persona Verifier.
+ * If assertion is valid store email in session  and check if user is already in db.
+ * If user isn't in db insert a new user document with an empty username property.
+ * If user is already in db just send 200 - OK.
+ */
 exports.login = function(req, resp) {
-
+  
+  var audience;
+  if (process.env.NODE_ENV === 'production') {
+    audience = 'http://mysterious-coast-9759.herokuapp.com/'
+  } else {
+    audience = 'http://localhost:3000/'
+  }
+  
   request.post({
     url: 'https://login.persona.org/verify',
     json: {
       assertion: req.body.assertion,
-      audience: 'http://mysterious-coast-9759.herokuapp.com/'
+      audience: audience
     }
   }, function(err, res, body) {
     console.log(body);
@@ -26,7 +42,6 @@ exports.login = function(req, resp) {
           };
           db.insert(doc, email, function(err, body) {
             if (err) console.log(err);
-            console.log(body);
             resp.send(200);
           })
         } else {
@@ -39,7 +54,14 @@ exports.login = function(req, resp) {
 
 };
 
+/**
+ * GET /auth/logout
+ *
+ * Route is part of Persona integration. It is called by navigator.id.watch -> onlogout.
+ * Destroy session, delete cookie and redirect user to index page.
+ */
 exports.logout = function(req, res) {
   req.session = null;
+  res.clearCookie('email');
   res.redirect('/');
 };
