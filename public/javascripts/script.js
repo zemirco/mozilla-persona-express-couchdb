fbf93c0f2e836a296e6482dc1673c1ee2191c5be
Mirco <mirco.zeiss@gmail.com>
2013-05-03 19:46:33 +0200

    rm logs, jquery post and twitter

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,9 +1,7 @@
 $(function() {
 
-  console.log('working');
-
-  $('#login').on('click', function() {
-    console.log('awesome');
+  $('#login').on('submit', function(event) {
+    event.preventDefault();
     navigator.id.request();
   });
 
@@ -12,19 +10,22 @@ $(function() {
     navigator.id.logout();
   });
 
-  var email = getEmail() || null;
-  console.log(email);
+  var email = $.cookie('email') || null;
 
+  // Mozilla Persona integration
   navigator.id.watch({
     loggedInUser: email,
     onlogin: function(assertion) {
-      $.post('/auth/login', {assertion: assertion}).success(function() {
+      var token = $('#token').val();
+      $.post('/auth/login', {assertion: assertion, _csrf: token}, function(data) {
         window.location.reload();
-      })
+      });
     },
     onlogout: function() {
       window.location = '/auth/logout';
     }
   });
-
+  
+  // twitter
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 });
