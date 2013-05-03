bffa9cb995ba7616c3c53afe372244b15bf848b4
Mirco <mirco.zeiss@gmail.com>
2013-05-03 19:43:02 +0200

    comments for db connection

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
 
 var nano = require('nano');
-// not included in repo - use your own CouchDB instance
+// user and pw for CouchDB are not included in repo - use your own instance
 var config = {};
 
 if (process.env.NODE_ENV === 'production') {
+  // use environment variables on heroku
   config.user = process.env.user;
   config.pw = process.env.pw;
 } else {
+  // use local config during development
   config = require('./config.prod.js');
 }
 
+// create database connection
 var db = nano('https://' + config.user + ':' + config.pw + '@mirco.cloudant.com/blog-persona-example/');
 
 module.exports = db;
